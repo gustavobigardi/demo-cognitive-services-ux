694305c5f2edc02d38ecc447790042d8e6f738e6
Gustavo Bellini Bigardi <gbbigardi@gmail.com>
2018-11-06 21:16:21 -0200

    "Logica" para reconhecer CNH. Faltam outros documentos

--- WebApi/Services/CadastroService.cs
+++ WebApi/Services/CadastroService.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
 using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
@@ -12,6 +15,8 @@ namespace WebApi.Services
     {
         private readonly IComputerVisionClient _cvs;
 
+        private const int numberOfCharsInOperationId = 36;
+
         public CadastroService(IComputerVisionClient cvs)
         {
             _cvs = cvs;
@@ -20,26 +25,47 @@ namespace WebApi.Services
         public async Task<Usuario> PreencherDados(Stream streamDocumento)
         {
             var result = await GetData(streamDocumento);
-            return new Usuario();
+            return PreencherUsuario(result);
         }
 
-        private async Task<Dictionary<string, string>> GetData(Stream streamImage)
+        private async Task<List<string>> GetData(Stream streamImage)
         {
-            OcrResult ocrResult = await _cvs.RecognizePrintedTextInStreamAsync(true, streamImage, OcrLanguages.Pt);
-            var result = new Dictionary<string, string>();
+            RecognizeTextInStreamHeaders headers = await _cvs.RecognizeTextInStreamAsync(streamImage, TextRecognitionMode.Printed);
 
-            result.Add("Key", OcrResultsToString(ocrResult));
+            string operationId = headers.OperationLocation.Substring(
+                headers.OperationLocation.Length - numberOfCharsInOperationId);
 
-            return result;
+            TextOperationResult result =
+               await _cvs.GetTextOperationResultAsync(operationId);
+
+            int i = 0;
+            int maxRetries = 10;
+            while ((result.Status == TextOperationStatusCodes.Running ||
+                    result.Status == TextOperationStatusCodes.NotStarted) && i++ < maxRetries)
+            {
+                await Task.Delay(1000);
+
+                result = await _cvs.GetTextOperationResultAsync(operationId);
+            }
+
+            return result.RecognitionResult.Lines.Select(l => l.Text).ToList();
         }
 
-        string OcrResultsToString(OcrResult result)
+        private Usuario PreencherUsuario(List<string> dados)
         {
-            return string.Join("\n",
-                result.Regions.ToList().Select(region =>
-                    string.Join(" ", region.Lines.ToList().Select(line =>
-                         string.Join(" ", line.Words.ToList().Select(word =>
-                             word.Text).ToArray())).ToArray())).ToArray());
+            Usuario usuario = new Usuario();
+
+            //CNH
+            if (dados.Any(x => x.Contains("CARTEIRA NACIONAL DE HAB")))
+            {
+                usuario.Nome = dados.ElementAt(dados.IndexOf("NOME")+1);
+                usuario.RG = dados.ElementAt(dados.IndexOf(dados.FirstOrDefault(x => x.Contains("DOC. IDENT")))+1).Split(" ").ElementAt(0);
+                usuario.OrgEmissor = dados.ElementAt(dados.IndexOf(dados.FirstOrDefault(x => x.Contains("DOC. IDENT")))+1).Split(" ").ElementAt(1);
+                usuario.CPF = dados.Where(x => Regex.IsMatch(x.Replace(" ", ""), @"^\d{3}\.\d{3}\.\d{3}-\d{2}$")).FirstOrDefault().Replace(" ","");
+                usuario.DataNascimento = DateTime.ParseExact(dados.ElementAt(dados.IndexOf(dados.FirstOrDefault(x => x.Contains("DATA NASC")))+2), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            }
+
+            return usuario;
         }
     }
 }
